c74b5507f3c506f5d1d59ed026d0a6b8bcaac0db
Akshat-07k <akshat070704@gmail.com>
2025-08-30 17:09:36 +0530

    gemini added

--- mailvas/src/pages/HomeView.vue
+++ mailvas/src/pages/HomeView.vue
@@ -1,7 +1,8 @@
 <template>
   <div style="padding: 20px; font-family: Arial;color: wheat;min-height:91vh;">
     <h1>Gemini <s >AI</s> API </h1>
-    <input type="text" v-model="promptText" class="promptInput"><br>
+    <input type="text" v-model="promptText" class="promptInput">
+    <input type="file" @change="handleFileChange" class="promptInput"/> <br/>
     <button @click="generateText" style="padding: 10px 20px; cursor: pointer;">
       ask API
     </button>
@@ -16,21 +17,19 @@
 
 <script setup>
 import { ref } from "vue";
-import { GoogleGenerativeAI } from "@google/generative-ai"; // legacy 
+// import { GoogleGenerativeAI } from "@google/generative-ai"; // legacy 
 import { GoogleGenAI } from "@google/genai";    // recommended
 import loader from "@/Component/loader.vue";
 
-const loaderVar = ref(false);  //
-
+const loaderVar = ref(false);  
 const ai = new GoogleGenAI({ apiKey: "AIzaSyCoc3Ehpb84gzPmH_6Jxw7qceIkDAIP4lQ" });
 
 const API_KEY = "AIzaSyCoc3Ehpb84gzPmH_6Jxw7qceIkDAIP4lQ";
-const genAI = new GoogleGenerativeAI("AIzaSyCoc3Ehpb84gzPmH_6Jxw7qceIkDAIP4lQ");
+// const genAI = new GoogleGenerativeAI("AIzaSyCoc3Ehpb84gzPmH_6Jxw7qceIkDAIP4lQ");
 
 const promptText = ref("Create a simple .....");
-
 const response = ref("");
-
+const imageData = ref(null);
 // const generateText = async () => {
 //   try {
 //     const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" }); // 'gemini-pro' 
@@ -47,10 +46,23 @@ const generateText = async () => {
   try {
     loaderVar.value = true;
     const result = await ai.models.generateContent({
-        model: "gemini-2.5-flash",
+        "model": "gemini-2.5-flash",
+        // contents: [
+        //     { type: "text", text: promptText.value }
+        // ]
         contents: [
-            { type: "text", text: promptText.value }
-        ]
+            {
+              parts: [
+                { text: promptText.value },
+                {
+                  inlineData: {
+                    mimeType: "image/jpeg",
+                    data: imageData.value
+                  }
+                }
+              ]
+            }
+          ],
     });
     const text = result.text;
     response.value = text;
@@ -59,9 +71,28 @@ const generateText = async () => {
   } catch (error) {
     console.error("Error generating text:", error);
     response.value = "Error: " + error.message;
+    loaderVar.value = false;
   }
 };
+const handleFileChange = async(event)=>{
+  const response =  new Promise((resolve, reject)=>{
+    const file = event.target.files[0];
+    if(!file) return resolve(null);
 
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onload = ()=>{
+      const base64String = reader.result.split(",")[1];
+      resolve(base64String);
+    }
+    reader.onerror = (error)=>reject(error)
+  })
+
+  imageData.value = await response;
+  console.log(imageData)
+
+}
 </script>
 
 <style scoped>
@@ -78,5 +109,6 @@ button {
     margin-bottom: 10px;
     border: 1px solid #ccc;
     border-radius: 4px;
+    margin-right:10px;
 }
 </style>
